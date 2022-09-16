67ff758075472abd300306a440077c990bca1d19
damonli1999 <111948337+damonli1999@users.noreply.github.com>
2022-09-15 19:55:02 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+def house():
+    width = float(input("Enter the width of the house: "))
+    length = float(input("Enter the length of the house: "))
+
+    area = length * width
+
+    (width + length) *2 
+    print("Area of the house is: " + str(area))
+house()
+
+from math import pi
+def circle():
+    r = float(input ("Input the radius of the circle: "))
+    
+    print("The area of the circle with radius " + str(r) + "is: " + str(pi*r**2))
+
+
+
